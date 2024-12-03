df4134eaaf587feced40726b20dbc269edef3fa9
Danny <dannydotdev@gmail.com>
2024-12-03 12:00:14 +0100

    using express static

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 //Entry point
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-let port = 3000;
+
+let port = 8000;
+/*
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let port = 8000; 
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "/about.html"));
+  });
+  */
+
+//USING THE EXPRESS STATIC MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public", "/about.html")));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
