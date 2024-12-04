dad9b8f1707d66e280f8439ced5c7c7556991708
Danny <dannydotdev@gmail.com>
2024-12-04 11:32:34 +0100

    Using express static server files and .env

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
-//Entry point
 import express from "express";
-import path, { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
+const port = process.env.PORT;
 
-let port = 8000;
 /*
+// This is required for using __dirname in ES module format
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+//To get the about page, just add /abouthtml to the url
+*/
 
-let port = 8000; 
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "/about.html"));
-  });
-  */
+let posts = [
+  {
+    id: 1,
+    title: "First Post",
+    content: "This is the first post.",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    content: "This is the second post.",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+    content: "This is the third post.",
+  },
+];
 
-//USING THE EXPRESS STATIC MIDDLEWARE
-app.use(express.static(path.join(__dirname, "public", "/about.html")));
-
+// Json api
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+// Start the server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
